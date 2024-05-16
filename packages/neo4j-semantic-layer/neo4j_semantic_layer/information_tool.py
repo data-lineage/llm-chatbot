3a9ad97bf804bcd778e5a5cca994e456404fe8c1
surajpv1 <suraj.pv@suketa.in>
2024-05-16 12:07:19 +0530

    Added a seperate description query for company entity

--- packages/neo4j-semantic-layer/neo4j_semantic_layer/information_tool.py
+++ packages/neo4j-semantic-layer/neo4j_semantic_layer/information_tool.py
@@ -10,14 +10,31 @@ from langchain.pydantic_v1 import BaseModel, Field
 from langchain.tools import BaseTool
 
 from neo4j_semantic_layer.utils import get_candidates, graph
+import logging
 
-description_query = """
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Owns stock relationship Used for Querying single Manager Node
+description_query1 = """
 MATCH (m:Manager)-[r:OWNS_STOCK_IN]->(t)
 WHERE m.managerCik =  $candidate OR m.managerName =  $candidate
 WITH m, r, t
 WITH "Manager Name: " + coalesce(m.managerName, "") + "\nManager Address: " + coalesce(m.managerAddress, "") + "\nStocks: " + coalesce(r.desc, "") AS context
-RETURN context LIMIT 1
+RETURN context LIMIT 25
 """
+
+# Used for querying to fetch list of Managers present in a Company
+description_query2 = """
+MATCH (c:Company) - [r:OWNS_STOCK_IN] -(m:Manager)
+WHERE c.companyName = $candidate
+WITH c,r,m
+WITH "Manager Name: " + coalesce(m.managerName, "") AS context
+RETURN context LIMIT 25
+"""
+
+
 
 # # Case insensitive
 # description_query = """
@@ -31,41 +48,6 @@ RETURN context LIMIT 1
 # RETURN context LIMIT 1
 # """
 
-#  manager address
-# description_query = """
-# MATCH (m:Manager)
-# WHERE toLower(m.managerCik) = toLower($candidate) OR toLower(m.managerName) = toLower($candidate)
-# MATCH (m)-[r:OWNS_STOCK_IN]-(t)
-# WITH m, type(r) as type, collect(coalesce(t.companyName, t.cusipId)) as names
-# WITH m, type+": "+reduce(s="", n IN names | s + n + ", ") as types
-# WITH m, collect(types) as contexts
-# WITH m, "type:" + labels(m)[0] + "\nManager Name: "+ coalesce(m.managerName) + "\nManager Address: " + coalesce(m.managerAddress) as context
-# RETURN context LIMIT 1
-# """
-
-# # Owns stock relationship
-# description_query = """
-# MATCH (m:Manager)
-# WHERE m.managerCik = $candidate OR m.managerName = $candidate
-# MATCH (m)-[r:OWNS_STOCK_IN]-(t)
-# WITH m, type(r) as type, collect(coalesce(t.companyName, t.cusipId)) as stocks
-# WITH m, type+": "+reduce(s="", n IN stocks | s + n + ", ") as types
-# WITH m, collect(types) as contexts
-# WITH m, "type:" + labels(m)[0] + "\nManager Name: "+ coalesce(m.managerName)+"\nManager Address"+ coalesce(m.managerAddress) + "\nStocks: " + types as context
-# RETURN context LIMIT 1
-# """
-
-# Filed form relationships
-# description_query1 = """
-# MATCH (c:Company)
-# WHERE c.companyName = $candidate OR c.cusipId = $candidate
-# MATCH (c)-[r:FILED]-(t)
-# WITH c, type(r) as type, collect(coalesce(t.names, t.source)) as names
-# WITH c, type+": "+reduce(s="", n IN names | s + n + ", ") as types
-# WITH c, collect(types) as contexts
-# RETURN "Type: " + labels(c)[0] + "\nCompany Name: " + coalesce(c.companyName, "") + "\nForm Source: " + contexts[0] LIMIT 1
-# """
-
 def get_information(entity: str, type: str) -> str:
     candidates = get_candidates(entity, type)
     if not candidates:
@@ -76,24 +58,46 @@ def get_information(entity: str, type: str) -> str:
             "Need additional information, which of these "
             f"did you mean: {newline + newline.join(str(d) for d in candidates)}"
         )
-    data = graph.query(
-        # description_query1,
-        description_query, params={"candidate": candidates[0]["candidate"]}
-    )
-    return data[0]["context"]
+    
+    description_query = None
+    if type.lower() == "manager":
+        description_query = description_query1
+    elif type.lower() == "company":
+        description_query = description_query2
+    else:
+            return "Invalid entity type. Please provide either 'Manager' or 'Company'."
+
+    try:
+        # data = graph.query(
+        #     description_query, params={"candidate": candidates[0]["candidate"]}
+        # )
+        # if not data:
+        #     return "No detailed information was found for the specified Manager or Company"
+        # return data[0]["context"]
+        data = []
+        for candidate in candidates:
+            query_result = graph.query(description_query, params={"candidate": candidate["candidate"]})
+            data.extend(query_result)
+        if not data:
+            return "No information found for the provided candidates."
+    # Assuming context is a string field in the query results
+        return "\n".join(result["context"] for result in data)
+    except Exception as e:
+        logger.error(f"Error querying the database: {e}")
+        return "An error occurred while fetching the information."
 
 
 class InformationInput(BaseModel):
-    entity: str = Field(description="movie or a person mentioned in the question")
+    entity: str = Field(description="Manager or a Company mentioned in the question")
     entity_type: str = Field(
-        description="type of the entity. Available options are 'movie' or 'person'"
+        description="type of the entity. Available options are 'Manager' or 'Company'"
     )
 
 
 class InformationTool(BaseTool):
     name = "Information"
     description = (
-        "useful for when you need to answer questions about various actors or movies"
+        "useful for when you need to answer questions about various Manager or Company"
     )
     args_schema: Type[BaseModel] = InformationInput
 
@@ -114,7 +118,3 @@ class InformationTool(BaseTool):
     ) -> str:
         """Use the tool asynchronously."""
         return get_information(entity, entity_type)
-
-# Provide me the Address of manager Royal from the database where entity type is Manager
-
-# What is the investment done by the Manager Quantitative Investment Management, LLC in a company NETAPP INC. where entity type Manager from database
